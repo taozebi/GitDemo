935d900869e933e59eb27346af33d65ac66e7eee
taozebi <taozebi@qq.com>
2018-03-29 10:08:35 +0800

    Modify MainActivity.java; add Log info;

--- app/src/main/java/com/taoze/gitdemo/MainActivity.java
+++ app/src/main/java/com/taoze/gitdemo/MainActivity.java
@@ -2,15 +2,17 @@ package com.taoze.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toast.makeText(this,"test code modify",Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"test for secode modify");
     }
 }
